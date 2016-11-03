eb64ae806f430a027c84bec0bf7ad296de3f6b40
Usama M.Amin <ukaarwa@gmail.com>
2016-11-02 21:03:13 -0700

    completed

---
+++ public/app.js
@@ -0,0 +1,9 @@
+
+    var numberToMultiply = prompt("Please Write Table:","0");
+    
+    for (i = 1; i < 21; i++) {
+        document.write(numberToMultiply  + " x " + i + "= "+ numberToMultiply*i + "<br>");
+
+    }
+
+
